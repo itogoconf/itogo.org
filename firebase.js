a3c217500491f9671e65e9bca3d05c9ea4b5faec
Guillaume EHRET <guillaume@dev-mind>
2018-05-23 09:04:56 +0200

    first commit

---
+++ firebase.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "apiKey": process.env.MIXTEEN_API_KEY,
+  "authDomain": process.env.MIXTEEN_AUTH_DOMAIN,
+  "databaseURL": process.env.MIXTEEN_DATABASE_URL,
+  "storageBucket": process.env.MIXTEEN_STORAGE_BUCKET,
+  "user": process.env.MIXTEEN_USER_MAIL,
+  "password": process.env.MIXTEEN_PASSWORD,
+  "messagingSenderId": process.env.MIXTEEN_SENDER_ID
+};
---
+++ src/js/main.js
@@ -0,0 +1,52 @@
+/* eslint-env browser */
+window.app = (function() {
+  'use strict';
+
+  function initSw(){
+
+    var isLocalhost = Boolean(window.location.hostname === 'localhost' ||
+      // [::1] is the IPv6 localhost address.
+      window.location.hostname === '[::1]' ||
+      // 127.0.0.1/8 is considered localhost for IPv4.
+      window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+      )
+    );
+
+    if ('serviceWorker' in navigator && (window.location.protocol === 'https:' || isLocalhost)) {
+      navigator.serviceWorker
+        .register('/sw.js', {scope: '/'})
+        .then(function (registration) {
+          console.log('ServiceWorker registration successful with scope: ', registration.scope)
+          registration.onupdatefound = function () {
+            var installingWorker = registration.installing;
+            installingWorker.onstatechange = function () {
+              switch (installingWorker.state) {
+                case 'installed':
+                  if (navigator.serviceWorker.controller) {
+                    console.log('new update available');
+                    location.reload(true);
+                  }
+                  break;
+
+                default:
+              }
+            }
+          }
+        })
+        .catch(function(e) {
+          console.error('Error during service worker registration:', e);
+      });
+    }
+  }
+
+  function loadLazyImages() {
+    const images = document.getElementsByClassName('dm-img--lazyload');
+    Array.from(images).forEach(function(image){
+      image.src= image.getAttribute('data-src')
+    });
+  }
+
+  initSw();
+  loadLazyImages();
+})();
